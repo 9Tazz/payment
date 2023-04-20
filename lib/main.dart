5ed25ed4eb1fbf4a641f8fa0317685403d82d84f
9Tazz <fair.zazaqqr@example.com>
2023-04-21 01:29:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+//import 'package:flutter_application_1/nagi1.dart';
+import 'package:flutter_application_1/add_wallet.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: (AddWallet()),
+    ),
+  );
+}
